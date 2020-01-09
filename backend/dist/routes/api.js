a7da8df850c0344fa61c873ded114a2c2653a5df
Moritz <moritz.ploss@gmail.com>
2020-01-09 17:27:50 +0100

    add logout route

--- backend/dist/routes/api.js
+++ backend/dist/routes/api.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var authentication_1 = require("../controllers/authentication");
+var index_1 = require("../logging/index");
 var express = require("express");
 var apiRouter = express.Router();
 exports.apiRouter = apiRouter;
@@ -8,4 +9,8 @@ apiRouter.post('/login', authentication_1.authenticateRequest);
 apiRouter.get('/validate-session', function (req, res) {
     var status = req.session.authorized ? 200 : 401;
     return res.status(status).send();
+});
+apiRouter.get('/invalidate-session', function (req, res) {
+    req.session.destroy(index_1.logger.error);
+    return res.send();
 });
--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,5 +1,7 @@
 import { authenticateRequest } from '../controllers/authentication';
 
+import { logger } from '../logging/index';
+
 import express = require('express');
 
 const apiRouter = express.Router();
@@ -9,6 +11,11 @@ apiRouter.post('/login', authenticateRequest);
 apiRouter.get('/validate-session', (req, res) => {
   const status = req.session.authorized ? 200 : 401;
   return res.status(status).send();
+});
+
+apiRouter.get('/invalidate-session', (req, res) => {
+  req.session.destroy(logger.error);
+  return res.send();
 });
 
 export { apiRouter };
